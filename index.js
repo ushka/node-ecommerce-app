43b1f855600dcfe2ff59783d623a23f76fcf5201
Uros Urosevic <urosevic1986@gmail.com>
2022-04-02 17:40:05 +0800

    adds basic signup form and example of manually parsing submitted form data

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('hi there')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordconfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post('/', (req, res) => {
+  req.on('data', data => {
+    const parsed = data.toString('utf8').split('&');
+    const formData = {};
+    for (let pair of parsed) {
+      const [key, value] = pair.split('=');
+      formData[key] = value;
+    }
+    console.log(formData);
+  });
+  res.send('Account created');
+});
+
+app.listen(3000, () => {
+  console.log('Listening')
+});
