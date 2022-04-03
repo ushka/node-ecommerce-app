33189ff851c3cc7874a345f09c63531414c33857
Uros Urosevic <urosevic1986@gmail.com>
2022-04-03 17:15:41 +0800

    adds randomID generator to assign to newly created users so we cannot have duplicate users

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
   constructor(filename) {
@@ -23,6 +24,7 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId();
     const records = await this.getAll();
     records.push(attrs);
     await this.writeAll(records);
@@ -30,6 +32,10 @@ class UsersRepository {
 
   async writeAll(records) {
     await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
   }
 }
 
