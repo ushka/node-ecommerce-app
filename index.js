179cc5671519b767e662f63e2c5551eaeb81a3a4
Uros Urosevic <urosevic1986@gmail.com>
2022-04-04 22:47:08 +0800

    adds signup, signout and signin routes using cookie-session

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cookieSession({
   keys: ['eqkl12lkjqwelkj2123jklj23']
 }));
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
     <div>
       Your id is: ${req.session.userId}
@@ -24,8 +24,7 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', async (req, res) => {
-  // console.log(req.body);
+app.post('/signup', async (req, res) => {
   const { email, password, passwordconfirmation } = req.body;
   
   const existingUser = await usersRepo.getOneBy({ email });
@@ -41,6 +40,41 @@ app.post('/', async (req, res) => {
   req.session.userId = user.id;
 
   res.send('Account created');
+});
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {  
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <button>Sign In</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if(!user) {
+    return res.send('Email not found.')
+  }
+
+  if(user.password !== password) {
+    return res.send('Invalid password.') 
+  }
+
+  req.session.userId = user.id;
+
+  res.send('You are signed in.');
 });
 
 app.listen(3000, () => {
