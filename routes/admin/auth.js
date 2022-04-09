771033bca8077681dfbe8b1b9ea87032e964a41f
Uros Urosevic <urosevic1986@gmail.com>
2022-04-09 11:48:26 +0800

    wires up signup validation logic to view

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -33,14 +33,16 @@ router.get('/signout', (req, res) => {
 });
 
 router.get('/signin', (req, res) => {  
-  res.send(signinTemplate());
+  res.send(signinTemplate({}));
 });
 
 router.post('/signin', 
   [requireEmailExists, requireValidPasswordForUser], 
   async (req, res) => {
     const errors = validationResult(req);    
-    console.log(errors);
+    if(!errors.isEmpty()) {
+      return res.send(signinTemplate({ errors }));
+    }
     
     const { email } = req.body;
 
--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,12 +1,23 @@
 const layout = require('../layout');
 
-module.exports = () => {
+const getError = (errors, prop) => {
+  // Note: try/catch is a shortcut, more robust way would be to do all the potential if statements to avoid undefined errors appearing
+  try {
+    return errors.mapped()[prop].msg;
+  } catch(err) {
+    return '';
+  }
+}
+
+module.exports = ({ errors }) => {
   return layout({
     content: 
       `<div>
         <form method="POST">
           <input name="email" placeholder="email" />
+          ${getError(errors, 'email')}
           <input name="password" placeholder="password" />
+          ${getError(errors, 'password')}
           <button>Sign In</button>
         </form>
       </div>`
