9a0c3c386fecbafdaf1a2e80442c9b5e0ab57960
Uros Urosevic <urosevic1986@gmail.com>
2022-04-09 16:24:13 +0800

    adds skeleton for product routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session')
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cookieSession({
   keys: ['eqkl12lkjqwelkj2123jklj23']
 }));
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log('Listening')
