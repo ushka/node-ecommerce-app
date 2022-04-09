39719fd475061465830a769fb5a961755bb962c5
Uros Urosevic <urosevic1986@gmail.com>
2022-04-09 11:36:51 +0800

    adds validation logic for /signin route

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -36,23 +36,44 @@ router.get('/signin', (req, res) => {
   res.send(signinTemplate());
 });
 
-router.post('/signin', async (req, res) => {
-  const { email, password } = req.body;
+router.post('/signin', 
+  [
+    check('email')
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('Must provide a valid email.')
+      .custom(async (email) => {
+        const user = await usersRepo.getOneBy({ email });
+        if(!user) {
+          throw new Error('Email not found.');
+        }
+      }),
+    check('password')
+      .trim()
+      .custom(async (password, {req}) => {
+        const user = await usersRepo.getOneBy({ email: req.body.email });
+        if(!user) {
+          throw new Error('Invalid password');
+        }
 
-  const user = await usersRepo.getOneBy({ email });
+        const validPassword = await usersRepo.comparePasswords(user.password, password)
+        if(!validPassword) {
+          throw new Error('Invalid password.');
+        }
+      })
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req);    
+    console.log(errors);
+    
+    const { email } = req.body;
 
-  if(!user) {
-    return res.send('Email not found.')
-  }
-  const validPassword = await usersRepo.comparePasswords(user.password, password)
+    const user = await usersRepo.getOneBy({ email });       
 
-  if(!validPassword) {
-    return res.send('Invalid password.') 
-  }
+    req.session.userId = user.id;
 
-  req.session.userId = user.id;
-
-  res.send('You are signed in.');
+    res.send('You are signed in.');
 });
 
 module.exports = router;
