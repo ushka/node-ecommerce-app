c1cef9efdd1c264ecb5586bd244f4d384b8edab1
Uros Urosevic <urosevic1986@gmail.com>
2022-04-10 16:18:05 +0800

    creates error handling middleware to reduce repeated code in auth and products repositories

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
+// const { check, validationResult } = require('express-validator');
+
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -13,13 +15,9 @@ router.get('/signup', (req, res) => {
 
 router.post('/signup', 
   [requireEmail, requirePassword, requirePasswordConfirmation], 
-  async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }))
-    }
-
-    const { email, password, passwordconfirmation } = req.body;            
+  handleErrors(signupTemplate),
+  async (req, res) => {    
+    const { email, password } = req.body;            
 
     const user = await usersRepo.create({ email, password });
     req.session.userId = user.id;
@@ -38,12 +36,8 @@ router.get('/signin', (req, res) => {
 
 router.post('/signin', 
   [requireEmailExists, requireValidPasswordForUser], 
-  async (req, res) => {
-    const errors = validationResult(req);    
-    if(!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-    
+  handleErrors(signinTemplate),
+  async (req, res) => {        
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email });       
