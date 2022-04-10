54f5ce1697e310a45ca655e6606fdb3c842c5a94
Uros Urosevic <urosevic1986@gmail.com>
2022-04-10 11:57:26 +0800

    adds skeleton for file upload, makes new product form use enctype of multipart form and wires up third party multipart NodeJS library "multer"

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {
 
@@ -16,9 +19,11 @@ router.get('/admin/products/new', (req, res) => {
 
 router.post('/admin/products/new', 
   [requireTitle, requirePrice],
+  upload.single('image'),
   (req, res) => {
     const errors = validationResult(errors);
-    console.log(errors);
+    
+    console.log(file);
 
     res.send('Product Submitted.');
   });
