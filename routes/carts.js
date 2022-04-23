d7c3e3b151387f4b4b4362f7a569f8d461b4b6a5
Uros Urosevic <urosevic1986@gmail.com>
2022-04-23 13:04:33 +0800

    adds logic on the /cart/products POST route to: (1) create cart if doesn't exist already, or increment cart if it exists and that given product has already been added

--- routes/carts.js
+++ routes/carts.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
 
 // receive POST request to add an item to cart
-router.post('/cart/products', (req, res) => {
-  console.log(req.body.productId);
+router.post('/cart/products', async(req, res) => {
+
+  // Figure out the cart, make a new one or does one exist
+  let cart;
+  if(!req.session.cartId) {
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+  // console.log(cart);
+
+  // Either increment quantity for existing product or add new products to items array in Carts repository
+  const existingItem = cart.items.find(item => item.id === req.body.productId);
+  if(existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+
+  await cartsRepo.update(cart.id, {
+    items: cart.items
+  });
 
   res.send('Product added to cart.');
 });
