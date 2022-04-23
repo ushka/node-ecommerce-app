6f41ffe2d35fca9d4d8b08601632c8ff78cc9ef9
Uros Urosevic <urosevic1986@gmail.com>
2022-04-23 12:51:05 +0800

    basic test for first post request of the carts route

--- routes/carts.js
+++ routes/carts.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 // receive POST request to add an item to cart
+router.post('/cart/products', (req, res) => {
+  console.log(req.body.productId);
+
+  res.send('Product added to cart.');
+});
 
 // receive GET request to show all items in cart
 
