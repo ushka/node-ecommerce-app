227c284bb36ee49b1b6bdf50e19d1e95c1ca864c
Uros Urosevic <urosevic1986@gmail.com>
2022-04-23 13:17:02 +0800

    adds logic for show all items in the cart and a basic view to show cart items

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartIndexTemplate = require('../views/carts/index');
 
 const router = express.Router();
 
@@ -14,7 +16,6 @@ router.post('/cart/products', async(req, res) => {
   } else {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
-  // console.log(cart);
 
   // Either increment quantity for existing product or add new products to items array in Carts repository
   const existingItem = cart.items.find(item => item.id === req.body.productId);
@@ -32,6 +33,22 @@ router.post('/cart/products', async(req, res) => {
 });
 
 // receive GET request to show all items in cart
+router.get('/cart', async(req, res) => {
+  if(!req.session.cartId) {
+    return res.redirect('/');
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for(let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+    
+    item.product = product;
+  }
+
+  res.send(cartIndexTemplate({ items: cart.items }));
+
+});
 
 // receive POST request to delete an item from a cart 
 
